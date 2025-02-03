0842bc41db1a81be7b43af3740fbee1df4fb84f8
AryanSingh2006 <44184amit@gmail.com>
2025-02-03 15:20:04 +0530

    the basic to do list id done but it is not stored in the local stograge

--- script.js
+++ script.js
@@ -8,9 +8,43 @@ addButton.onclick=()=>{
     if(task !==""){
         const li=document.createElement("li");
         li.classList.add("task");
-        li.innerText=task;
+
+        const circleDiv=document.createElement("div");
+        circleDiv.classList.add("circle");
+         
+        const trashImg=document.createElement("img");
+        trashImg.src="trashCan.jpg";
+        trashImg.alt="trashCan";
+        trashImg.classList.add("trashCan")
+
+        taskList.append(li);
+
+        li.append(circleDiv,task,trashImg);
+        
+        trashImg.onclick=()=>{
+            li.remove();
+        }
+
+        circleDiv.addEventListener("click", ()=>{
+            if(li.style.textDecoration == "line-through"){
+                li.style.textDecoration="none";
+            }
+            else{
+                li.style.textDecoration = "line-through";
+            }
+        })
+
+        inputBox.value="";
     }
     else{
-        console.log("Please enter a task");
+        alert("Please enter a task");
     }
 }
+
+
+addButton.addEventListener("mouseover", ()=>{
+    addButton.style.opacity="0.7";
+})
+addButton.addEventListener("mouseout",()=>{
+    addButton.style.opacity="1";
+})
